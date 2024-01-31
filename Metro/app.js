dcadcc7a60fa4b5e9872d5e561d9ec9592ed4a38
Anshu Kumar Ranjan <52268442+meritaqatmerafaisla@users.noreply.github.com>
2024-01-31 11:42:07 +0530

    Add files via upload

---
+++ Metro/app.js
@@ -0,0 +1,20 @@
+// add sticky navbar
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY>0);
+})
+//activate the hamburger menu and mobile navigation
+const hamb=document.querySelector(".hamb");
+const nav=document.querySelector(".mobile-nav");
+hamb.addEventListener("click",function(){
+    this.classList.toggle("active");
+    nav.classList.toggle("slide");
+})
+//website scroll reveal effect
+const sr=ScrollReveal({
+    distance:"85px",
+    duration:2000,
+    reset:false,
+});
+sr.reveal(".hero .desc",{delay:250, origin:"bottom" });
+sr.reveal(".newsletter .content",{delay:250, origin:"bottom" });
